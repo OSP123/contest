18e0ba45b87b413d6e003f3f3edd6c1ba5946afa
Omar Patel <omarpatel123@gmail.com>
2017-10-06 01:11:32 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Can also be included with a regular script tag
+
+var options = {
+  strings: ["<h3>++INCOMING TRANSMISSION++</h3>","<h3>SOURCE: Roboute Gulliman,  Lord Commander and Imperial Regent, the living voice of the Emperor.</h3>", "<h3>AUTHORIZATION: [INQUISITOR GREYFAX]</h3>", "<h3>++ACCESS GRANTED—EYES ONLY++</h3>", "<h3>BEGIN CODED MESSAGE:</h3>"],
+  typeSpeed: 40
+}
+
+var typed = new Typed(".element", options);
