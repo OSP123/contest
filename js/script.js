bf58fe906513887d85039c17aaa925c50f3ad7a0
Omar Patel <omarpatel123@gmail.com>
2017-10-08 20:30:18 -0700

    update assets

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ var options = {
 }
 
 var typed = new Typed(".element", options);
+
+var currentDate = new Date();
+				// Set some date in the future. In this case, it's always Jan 1
+var futureDate  = new Date("October 22, 2017 11:59:59");
+// Calculate the difference in seconds between the future and current date
+var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+// Instantiate a coutdown FlipClock
+clock = $('.your-clock').FlipClock(diff, {
+	clockFace: 'DailyCounter',
+	countdown: true
+});
